24084a385aff8461e27a3862c3dc874da08869a4
cnickens <cnickens@smartinnovators.space>
2025-11-15 14:04:00 +0000

    fix: Remove conflicting app.listen()

--- functions/index.js
+++ functions/index.js
@@ -214,8 +214,3 @@ app.get('/trips/:tripId/reservations', getItemsFromTrip('reservations'));
 
 // Export the express app as a single Cloud Function
 exports.api = functions.https.onRequest(app);
-
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-    console.log(`API listening on port ${port}`);
-});
